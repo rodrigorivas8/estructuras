cae6e1c0a9bf4653fd124fc6fec90c5c8de6ed0e
rodrigorivas8 <165713150+rodrigorivas8@users.noreply.github.com>
2024-05-14 11:53:32 -0500

    Add files via upload

--- stack.h
+++ stack.h
@@ -1,42 +1,36 @@
-#ifndef SEM4_STACK_H
-#define SEM4_STACK_H
-#endif
-
+template <class type>
 class stack{
     int max;
-    int *list=new int[max];
-    int *tip=list-1;
+    type *list=new type[max];
+    type *tip=list-1;
 public:
+
     stack(int m):max(m){}
-    bool push(int);
-    int pop();
-    int top();
-    bool empty();
-    bool full();
+
+    bool push(type data){
+        if(tip==list+max-1) return false;
+        tip++;
+        *tip=data;
+        return true;
+    }
+
+    type pop(){
+        if(tip==list-1) return -1;
+        tip--;
+        return *(tip+1);
+    }
+
+    type top(){
+        if(tip==list-1) return -1;
+        return *tip;
+    }
+
+    bool empty(){
+        return tip==list-1;
+    }
+
+    bool full(){
+        return tip==list+max-1;
+    }
+
 };
-
-bool stack::push(int data){
-    if(tip==list+max-1) return false;
-    tip++;
-    *tip=data;
-    return true;
-}
-
-int stack::pop(){
-    if(tip==list-1) return -1;
-    tip--;
-    return *(tip+1);
-}
-
-int stack::top(){
-    if(tip==list-1) return -1;
-    return *tip;
-}
-
-bool stack::empty(){
-    return tip==list-1;
-}
-
-bool stack::full(){
-    return tip==list+max-1;
-}
