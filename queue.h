305562743472549ed0808b508184d992c5eed4b6
rodrigorivas8 <165713150+rodrigorivas8@users.noreply.github.com>
2024-05-14 11:22:20 -0500

    Add files via upload

--- queue.h
+++ queue.h
@@ -12,52 +12,41 @@ public:
         tail=head;
     };
 
-    bool empty() const;
-    bool full() const;
-    type front() const;
-    void enqueue(type);
-    type dequeue();
-};
+    bool empty()const{
+        return head==tail;
+    }
 
-template <class type>
-bool queue<type>::empty()const{
-    return head==tail;
-}
+    bool full()const{
+        int h=int(head-list),
+                t=int(tail-list);
+        return h==(t+1)%max;
+    }
 
-template <class type>
-bool queue<type>::full()const{
-    int h=int(head-list),
-        t=int(tail-list);
-    return h==(t+1)%max;
-}
+    type front()const{
+        if(head==tail)
+            return -1;
+        return *head;
+    }
 
-template <class type>
-type queue<type>::front()const{
-    if(head==tail)
-        return -1;
-    return *head;
-}
+    void enqueue(type data){
+        int h=int(head-list),
+                t=int(tail-list);
+        *tail=data;
+        t=(t+1)%max;
+        if(h==t){
+            h=(h+1)%max;
+            head=list+h;
+        }
+        tail=list+t;
+    }
 
-template <class type>
-void queue<type>::enqueue(type data){
-    int h=int(head-list),
-            t=int(tail-list);
-    *tail=data;
-    t=(t+1)%max;
-    if(h==t){
+    type dequeue(){
+        int h=int(head-list),
+                t=int(tail-list);
+        type r=*head;
+        if(h==t) return nullptr;
         h=(h+1)%max;
         head=list+h;
+        return r;
     }
-    tail=list+t;
-}
-
-template <class type>
-type queue<type>::dequeue(){
-    int h=int(head-list),
-        t=int(tail-list);
-    type r=*head;
-    if(h==t) return nullptr;
-    h=(h+1)%max;
-    head=list+h;
-    return r;
-}
+};
